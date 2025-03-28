fca2d18f63cc5c929af512a53fa0aa83926d18cf
Felix Nguyen <pnguyen72@my.bcit.ca>
2025-03-28 10:50:14 -0700

    Add javadoc

--- src/code/ca/bcit/comp2522/lab10/Bank.java
+++ src/code/ca/bcit/comp2522/lab10/Bank.java
@@ -3,20 +3,44 @@ package ca.bcit.comp2522.lab10;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Represents a bank that manages a list of bank accounts.
+ * Provides functionality to add accounts, retrieve accounts by ID,
+ * and calculate the total balance of all accounts in USD.
+ *
+ * @author Brownie, Felix
+ * @version 1
+ */
 public class Bank
 {
     private final List<BankAccount> accountList;
 
+    /**
+     * Constructs a new Bank object.
+     * Initializes an empty list to store accounts.
+     */
     public Bank()
     {
         accountList = new ArrayList<>();
     }
 
+    /**
+     * Adds a new bank account to the bank.
+     *
+     * @param account the BankAccount object to be added
+     */
     public void addAccount(final BankAccount account)
     {
         accountList.add(account);
     }
 
+    /**
+     * Retrieves a bank account by its account ID.
+     *
+     * @param accountID the ID of the account to retrieve
+     * @return the BankAccount object with the specified ID
+     * @throws IllegalArgumentException if the account is not found
+     */
     public BankAccount retrieveAccount(final String accountID)
     {
         for(final BankAccount account : accountList)
@@ -29,6 +53,11 @@ public class Bank
         throw new IllegalArgumentException("Account not found");
     }
 
+    /**
+     * Calculates the total balance of all accounts in USD.
+     *
+     * @return the total balance in USD
+     */
     public double totalBalanceUsd()
     {
         return accountList.stream()
--- src/code/ca/bcit/comp2522/lab10/BankAccount.java
+++ src/code/ca/bcit/comp2522/lab10/BankAccount.java
@@ -1,10 +1,29 @@
 package ca.bcit.comp2522.lab10;
 
+/**
+ * Represents a bank account with basic operations such as deposit, withdraw,
+ * and transfer. Each account is identified by a unique account ID and maintains
+ * a balance.
+ *
+ * @author Brownie, Felix
+ * @version 1
+ */
 public class BankAccount
 {
     private       double balance;
     private final String accountID;
 
+    /**
+     * Constructs a BankAccount with a specified account ID and initial balance.
+     *
+     * @param accountID      the unique identifier for the account, must be
+     *                       non-null,
+     *                       non-empty, and contain only numeric characters.
+     * @param initialBalance the initial balance of the account, must be
+     *                       non-negative.
+     * @throws IllegalArgumentException if the account ID is invalid or the initial
+     *                                  balance is negative.
+     */
     public BankAccount(final String accountID, final int initialBalance)
     {
         validateAccountID(accountID);
@@ -13,11 +32,21 @@ public class BankAccount
         this.balance   = initialBalance;
     }
 
+    /**
+     * Retrieves the current balance of the account in USD.
+     *
+     * @return the current balance of the account.
+     */
     protected double getBalanceUsd()
     {
         return balance;
     }
 
+    /**
+     * Retrieves the unique account ID of the account.
+     *
+     * @return the account ID.
+     */
     protected String getAccountID()
     {
         return accountID;
@@ -44,13 +73,27 @@ public class BankAccount
         }
     }
 
+    /**
+     * Deposits a specified amount into the account.
+     *
+     * @param amount the amount to deposit, must be non-negative.
+     * @throws IllegalArgumentException if the amount is negative.
+     */
     public void deposit(final double amount)
     {
         validateAmount(amount);
         this.balance += amount;
     }
 
-
+    /**
+     * Withdraws a specified amount from the account.
+     *
+     * @param amount the amount to withdraw, must be non-negative and less than
+     *               or equal to the current balance.
+     * @return the withdrawn amount.
+     * @throws IllegalArgumentException if the amount is negative or exceeds
+     *                                  the current balance.
+     */
     public double withdraw(final double amount)
     {
         validateAmount(amount);
@@ -62,6 +105,18 @@ public class BankAccount
         return amount;
     }
 
+    /**
+     * Transfers a specified amount from this account to another bank account.
+     *
+     * @param account   the target bank account to transfer funds to.
+     * @param accountID the account ID of this account, must match this account's
+     *                  ID.
+     * @param amount    the amount to transfer, must be non-negative and less than
+     *                  or equal to the current balance.
+     * @throws IllegalArgumentException if the account ID does not match this
+     *                                  account's ID, the amount is negative, or
+     *                                  the amount exceeds the current balance.
+     */
     public void transferToBank(final BankAccount account,
                                final String accountID,
                                final int amount)
